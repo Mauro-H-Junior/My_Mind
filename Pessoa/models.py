2ddfa8cad43373eec1ed0d497798334a980aae74
mauro-jr@outlook.com <mauro-jr@outlook.com>
2023-01-28 14:09:23 -0300

    commit_mj

--- Pessoa/models.py
+++ Pessoa/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+tipo_escolhas_avaliacao_diaria = {
+    ('PÉSSIMO', 'PÉSSIMO SUPORTE'),    
+    ('RUIM', 'RUIM'),    
+    ('BOM', 'BOM'),
+    ('EXCELENTE', 'EXCELENTE'),
+
+}
+
+tipo_escolhas_alerta_cristes = {
+    ('PANICO', 'PANICO'),    
+    ('ANSIEDADE', 'ANSIEDADE'),    
+    ('DEPRESSAO DE CURSO', 'DEPRESSAO'),
+}
+
+acontecimento_negativo = models.TextField("Assunto do chamado",max_length=100)
+acontecimento_positivo = models.TextField("Assunto do chamado",max_length=100)
